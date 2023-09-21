af9a5fe10bb22ac3c36eb0906c931ae8244b2ac4
arigsela <arigsela@gmail.com>
2023-09-21 16:59:15 -0400

    adding validations

--- project-2/books.py
+++ project-2/books.py
@@ -1,4 +1,6 @@
-from fastapi import Body, FastAPI
+from typing import Optional
+from fastapi import FastAPI
+from pydantic import BaseModel, Field
 
 app = FastAPI()
 
@@ -18,6 +20,13 @@ class Book:
         self.rating = rating
 
 
+class BookRequest(BaseModel):
+    id: Optional[int]
+    title: str = Field(min_length=3)
+    author: str = Field(min_length=1)
+    description: str = Field(min_lenth=1, max_length=100)
+    rating: int = Field(gt=1, lt=6)
+
 
 BOOKS = [
     Book(1, "Computer Science PRo", "CodingWithRoby", "Very nice book", 5),
@@ -34,3 +43,26 @@ BOOKS = [
 @app.get("/books")
 async def read_all_books():
     return BOOKS
+
+
+# POST requests #
+
+
+@app.post("/create-book")
+async def create_book(book_request: BookRequest):
+    # Converting request to Book object
+    new_book = Book(**book_request.model_dump())
+    BOOKS.append(find_book_id(new_book))
+
+
+def find_book_id(book: Book):
+    #Ternary operator
+    #book.id = 1 if len(BOOKS) == 0 else BOOKS[-1].id + 1
+
+    if len(BOOKS) > 0:
+        # Grab the last element in the list
+        book.id = BOOKS[-1].id + 1
+    else:
+        book.id = 1
+
+    return book
